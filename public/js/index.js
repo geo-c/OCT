1a0cdb0229c9decf3cc4b9387efa562b8d855c40
Jonas Wissing <j_wiss04@uni-muenster.de>
2016-07-08 11:06:08 +0200

    Added 'More' Functionality for the statistics

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	var url = "api/";
-
+	var table = null;
+	var graph = null;
 
 	//Init
 	Apps();
@@ -22,38 +23,152 @@ $(document).ready(function() {
 
 	function Apps() {
 		$("#content").empty();
-		$("#content").html('<table id="table" class="display" style="width=100%;"><thead><tr><th>Name</th><th>Description</th><th>Calls</th></tr></thead><tbody></tbody></table>');
-		initTable();
+		$("#content").html('<table id="table" class="display" style="width=100%;"><thead><tr><th style=\"display:none;\"></th><th class=\"details-control sorting_disabled headrow\" rowspan=\"1\" colspan=\"1\" aria-label=\"\" style=\"width: 18px;\">More</th><th>Name</th><th>Description</th><th>Calls</th></tr></thead><tbody></tbody></table>');
+		initTableApps();
 	}
 
 	function Tags() {
 		$("#content").empty();
+		$("#content").html('<table id="table" class="display" style="width=100%;"><thead><tr><th style=\"display:none;\"><th class=\"details-control sorting_disabled headrow\" rowspan=\"1\" colspan=\"1\" aria-label=\"\" style=\"width: 18px;\">More</th><th>Name</th><th>Calls</th></tr></thead><tbody></tbody></table>');
+		initTableTags();
 	}
 
 	function Categories() {
 		$("#content").empty();
+		$("#content").html('<table id="table" class="display" style="width=100%;"><thead><tr><th style=\"display:none;\"><th class=\"details-control sorting_disabled headrow\" rowspan=\"1\" colspan=\"1\" aria-label=\"\" style=\"width: 18px;\">More</th><th>Name</th><th>Calls</th></tr></thead><tbody></tbody></table>');
+		initTableCategories();
 	}
 
 	function Usage() {
 		$("#content").empty();
 		$("#content").html('<div style="width=600px;height=400px;"><canvas id="grid"></canvas><div>');
-		initGraph();
+		initGraphUsage();
 	}
 
-	function initTable() {
+	function initTableApps() {
 		$.getJSON(url + "apps", function(json){
 			for(index in json) {
 				var $bline = $( "<tr></tr>" );
-				$bline.append( $( '<td></td>' ).html( json[index].app_name ) );
+				$bline.append( $( "<td class=\"app_hash\" style=\"display:none;\"></td>").html(json[index].app_hash) );
+				$bline.append( $( "<td class=\"details-control\"></td>" ).html('') );
+				$bline.append( $( "<td></td>" ).html( json[index].app_name ) );
 				$bline.append( $( "<td></td>" ).html( json[index].app_description ) );
 				$bline.append( $( "<td></td>" ).html( json[index].calls ) );
 				$('#table').append($bline);
 			}
-			$('#table').DataTable();
+			table = $('#table').DataTable();
 		});	
+
+		$('#table tbody').on('click', 'td.details-control', function () {
+	        var tr = $(this).closest('tr');
+	        var row = table.row( tr );
+	 
+	        if ( row.child.isShown() ) {
+	            // This row is already open - close it
+	            row.child.hide();
+	            tr.removeClass('shown');
+	        }
+	        else {
+	            // Open this row
+	            row.child( appMoreInfo(row.data()[0]) ).show();
+	            tr.addClass('shown');
+	        }
+	    } );
 	}
 
-	function initGraph() {
+	function appMoreInfo(app_hash) {
+		$.getJSON(url + "apps/" + app_hash + '/logsByTag', function(json){
+			for(index in json) {
+				$('#tags').append('<tr><td>'+json[index].tag_name+'</td><td>'+ json[index].count +'</td></tr>');
+			}
+		});
+		$.getJSON(url + "apps/" + app_hash + '/logsByCategory', function(json){
+			for(index in json) {
+				$('#categories').append('<tr><td>'+json[index].catgegory_name+'</td><td>'+ json[index].count +'</td></tr>');
+			}
+		});
+		return '<table id="detail"><thead><th>Tags</th><th>Categories</th></thead><tr><td id="tags"></td><td id="categories"></td></tr></table>';
+	}
+
+	function initTableTags() {
+		$.getJSON(url + "tags", function(json){
+			for(index in json) {
+				var $bline = $( "<tr></tr>" );
+				$bline.append( $( "<td class=\"app_hash\" style=\"display:none;\"></td>").html(json[index].tag_id) );
+				$bline.append( $( "<td class=\"details-control\"></td>" ).html('') );
+				$bline.append( $( '<td></td>' ).html( json[index].tag_name ) );
+				$bline.append( $( "<td></td>" ).html( json[index].calls ) );
+				$('#table').append($bline);
+			}
+			table = $('#table').DataTable();
+		});	
+
+		$('#table tbody').on('click', 'td.details-control', function () {
+	        var tr = $(this).closest('tr');
+	        var row = table.row( tr );
+	 
+	        if ( row.child.isShown() ) {
+	            // This row is already open - close it
+	            row.child.hide();
+	            tr.removeClass('shown');
+	        }
+	        else {
+	            // Open this row
+	            row.child( tagMoreInfo(row.data()[0]) ).show();
+	            tr.addClass('shown');
+	        }
+	    } );
+	}
+
+	function tagMoreInfo(tag_id) {
+		$.getJSON(url + "tags/" + tag_id + '/apps', function(json){
+			for(index in json) {
+				$('#app').append('<tr><td>'+json[index].app_name+'</td><td>'+ json[index].count +'</td></tr>');
+			}
+		});
+		return '<table id="detail"><thead><th>App</th><th>Calls</th></thead><tr><td id="app"></td><td id="Calls"></td></tr></table>';
+	}
+
+	function initTableCategories() {
+		$.getJSON(url + "categories", function(json){
+			for(index in json) {
+				var $bline = $( "<tr></tr>" );
+				$bline.append( $( "<td class=\"app_hash\" style=\"display:none;\"></td>").html(json[index].category_id) );
+				$bline.append( $( "<td class=\"details-control\"></td>" ).html('') );
+				$bline.append( $( '<td></td>' ).html( json[index].catgegory_name ) );
+				$bline.append( $( "<td></td>" ).html( json[index].calls ) );
+				$('#table').append($bline);
+			}
+			table = $('#table').DataTable();
+		});
+
+		$('#table tbody').on('click', 'td.details-control', function () {
+	        var tr = $(this).closest('tr');
+	        var row = table.row( tr );
+	 
+	        if ( row.child.isShown() ) {
+	            // This row is already open - close it
+	            row.child.hide();
+	            tr.removeClass('shown');
+	        }
+	        else {
+	            // Open this row
+	            row.child( categoryMoreInfo(row.data()[0]) ).show();
+	            tr.addClass('shown');
+	        }
+	    } );
+	}
+
+	function categoryMoreInfo(category_id) {
+		$.getJSON(url + "categories/" + category_id + '/apps', function(json){
+			for(index in json) {
+				$('#app').append('<tr><td>'+json[index].app_name+'</td><td>'+ json[index].count +'</td></tr>');
+			}
+		});
+		return '<table id="detail"><thead><th>App</th><th>Calls</th></thead><tr><td id="app"></td><td id="Calls"></td></tr></table>';
+	}
+
+	function initGraphUsage() {
 		$.getJSON(url + "logs/countByDay", function (json) {
 			var ctx = $("#grid")
 			var labels = [];
