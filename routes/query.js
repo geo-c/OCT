c680cfce68c246941ff90207891c2693a588f35f
Jonas Wissing <j_wiss04@uni-muenster.de>
2016-10-04 10:18:41 +0200

    fix #1

--- routes/query.js
+++ routes/query.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-var queryTag = require('../controllers/query/tag');
 var queryCategory = require('../controllers/query/category');
 var point = require('../controllers/query/point');
 var getDataset = require('../controllers/query/getDataset');
 var check = require('../controllers/query/check');
 
-// Query
-router.get('/query/tag/:tag_name', queryTag.request);
 
 router.get('/query/:category_name', queryCategory.request);
 
